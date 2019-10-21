051719e9cb6c2e74dfe92ce0c4a5217eb9c3ed62
toby <tobmag1@gmail.com>
2019-10-21 15:53:12 +0100

    first commit

---
+++ src/app/scrumuser.ts
@@ -0,0 +1,13 @@
+export class Scrumuser {
+    constructor(
+        public fullname: string,
+        public email: string,
+        public password: string,
+        public projname: string,
+        public type: string,
+        
+        
+    
+    ) {}
+        
+}
